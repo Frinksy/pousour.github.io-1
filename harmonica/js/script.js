aefd30005d121e5b27587fe0a83144dc98706790
Adam Blanchet <adamblanchet@free.fr>
2021-03-16 15:38:16 +0100

    stop relying on wav files

--- harmonica/js/script.js
+++ harmonica/js/script.js
@@ -29,70 +29,135 @@ for (var i = 0; i < 10; i++) {
 	document.body.appendChild(audio);
 }
 
+// function playNote(index) {
+// 	document.getElementById(index).style.backgroundColor = "#a6a6a6";
+// 	index++;
+// 	if (blow) index += "a";
+// 	else index += "b"
+// 	var audio = document.getElementById(index);
+// 	audio.currentTime = 0;
+// 	audio.volume = volume;
+// 	audio.play();
+// }
+
+// function stopNote(index) {
+// 	document.getElementById(index).style.backgroundColor = "#808080";
+// 	index++;
+// 	var audio = document.getElementById(index+"a");
+// 	audio.pause();
+// 	audio.currentTime = 0;
+
+// 	var audio = document.getElementById(index+"b");
+// 	audio.pause();
+// 	audio.currentTime = 0;
+// }
+
+// addEventListener('keydown', function(event) {
+// 	if (event.repeat) return
+// 	var index = KEYS.indexOf(event.key);
+
+// 	if (index != -1) playNote(index);
+// 	else if (event.key == " ") {
+// 		var listAudio = document.querySelectorAll('audio');
+// 		for (var i = 0; i < listAudio.length; i++) {
+// 			if (!listAudio[i].paused) {
+// 				var index = listAudio[i].id;
+// 				index = index.substr(0, index.length - 1);
+// 				index = index - 1;
+// 				stopNote(index);
+// 				blow = false;
+// 				playNote(index);
+// 			}
+// 		}
+// 		blow = false;
+// 	} 
+// });
+
+// addEventListener('keyup', function(event) {
+// 	var index = KEYS.indexOf(event.key);
+
+// 	if (index != -1) stopNote(index);
+// 	else if (event.key == " ") {
+// 		var listAudio = document.querySelectorAll('audio');
+// 		for (var i = 0; i < listAudio.length; i++) {
+// 			if (!listAudio[i].paused) {
+// 				var index = listAudio[i].id;
+// 				index = index.substr(0, index.length - 1);
+// 				index = index - 1;
+// 				stopNote(index);
+// 				blow = true;
+// 				playNote(index);
+// 			}
+// 		}
+// 		blow = true;
+// 	} 
+// });
+
+
+const frequencies = [440, 493.88, 523.25, 587.3, 659.25, 698.46, 783.99] // A4 -> G6
+
+let oscillatorNodes = []
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)({latencyHint: "interactive"});
+// Setup oscillatorNodes
+for (let i = 0; i < frequencies.length; i++) {
+    let osc = audioCtx.createOscillator();
+    osc.frequency.setValueAtTime(frequencies[i]/4, audioCtx.currentTime);
+
+	real = new Float32Array([1.0, 0.98, 2.12, 0.19, 0.2, 0.22, 0.52, 0.3, 0.19, 0.01, 0.02, 0.03, 0.01, 0.00, 0.004, 0, 0, 0, 0, 0]);
+	imag = new Float32Array(real.length)
+
+	wave = audioCtx.createPeriodicWave(real, imag);
+	osc.setPeriodicWave(wave);
+
+    osc.connect(audioCtx.destination);
+    oscillatorNodes.push(
+        osc
+    );
+}
+
 function playNote(index) {
-	document.getElementById(index).style.backgroundColor = "#a6a6a6";
-	index++;
-	if (blow) index += "a";
-	else index += "b"
-	var audio = document.getElementById(index);
-	audio.currentTime = 0;
-	audio.volume = volume;
-	audio.play();
+
+    oscillatorNodes[index].start();
+
 }
 
 function stopNote(index) {
-	document.getElementById(index).style.backgroundColor = "#808080";
-	index++;
-	var audio = document.getElementById(index+"a");
-	audio.pause();
-	audio.currentTime = 0;
+    
+	oscillatorNodes[index].stop();
+	let osc = audioCtx.createOscillator();
+	osc.frequency.setValueAtTime(frequencies[index]/4, audioCtx.currentTime);
+	osc.connect(audioCtx.destination);
+	real = new Float32Array([1.0, 0.98, 2.12, 0.19, 0.2, 0.22, 0.52, 0.3, 0.19, 0.01, 0.02, 0.03, 0.01, 0.00, 0.004, 0, 0, 0, 0, 0]);
+	imag = new Float32Array(real.length)
 
-	var audio = document.getElementById(index+"b");
-	audio.pause();
-	audio.currentTime = 0;
+	wave = audioCtx.createPeriodicWave(real, imag);
+	osc.setPeriodicWave(wave);
+	
+	oscillatorNodes[index] = osc;
+
 }
 
-addEventListener('keydown', function(event) {
+addEventListener('keydown', function (event) {
 	if (event.repeat) return
-	var index = KEYS.indexOf(event.key);
+	console.log("keydown");
+	console.log(event.key);
+	
+	if (KEYS.indexOf(event.key) != -1) {
+		console.log("bingo");
+		var index = KEYS.indexOf(event.key);
+		console.log(index);
+		playNote(index);
+	}
 
-	if (index != -1) playNote(index);
-	else if (event.key == " ") {
-		var listAudio = document.querySelectorAll('audio');
-		for (var i = 0; i < listAudio.length; i++) {
-			if (!listAudio[i].paused) {
-				var index = listAudio[i].id;
-				index = index.substr(0, index.length - 1);
-				index = index - 1;
-				stopNote(index);
-				blow = false;
-				playNote(index);
-			}
-		}
-		blow = false;
-	} 
 });
 
-addEventListener('keyup', function(event) {
-	var index = KEYS.indexOf(event.key);
-
-	if (index != -1) stopNote(index);
-	else if (event.key == " ") {
-		var listAudio = document.querySelectorAll('audio');
-		for (var i = 0; i < listAudio.length; i++) {
-			if (!listAudio[i].paused) {
-				var index = listAudio[i].id;
-				index = index.substr(0, index.length - 1);
-				index = index - 1;
-				stopNote(index);
-				blow = true;
-				playNote(index);
-			}
-		}
-		blow = true;
-	} 
+addEventListener('keyup', function (event) {
+	if (event.repeat) return
+	if (KEYS.indexOf(event.key) != -1) {
+		var index = KEYS.indexOf(event.key);
+		stopNote(index);
+	}
 });
-
 
 
 const HAPPY_BIRTHDAY = [6, 6, -6, 6, 7, -7, 6, 6, -6, 6, -8, 7, 6, 6, 9, 8, 7, -7, -6, -9, -9, 8, 7, -8, 7];
